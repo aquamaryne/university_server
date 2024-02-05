33bbe64bf36cb37099d7d4e93d3bc48ca25065a1
aquamaryne <67910638+aquamaryne@users.noreply.github.com>
2024-02-05 16:42:55 +0200

    changes in design

--- front/src/App.js
+++ front/src/App.js
@@ -1,15 +1,18 @@
 import Authorizations from "./components/keyAuth";
 import MainPage from "./components/mainPage";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import "../src/css/theme.css";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Authorizations />} />
-        <Route path="/components/mainPage" element={<MainPage />} />
-      </Routes>
-    </Router>   
+    <div className="theme">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Authorizations />} />
+          <Route path="/components/mainPage" element={<MainPage />} />
+        </Routes>
+      </Router> 
+    </div>  
   );
 }
 
--- front/src/components/keyAuth.js
+++ front/src/components/keyAuth.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { TextField, Button, Typography, Container, Box } from '@mui/material';
+import { fontFamily } from '@mui/system';
 
 const Authorizations = () => {
     const [key, setKey] = useState('');
@@ -11,6 +13,8 @@ const Authorizations = () => {
         try{
             const responce = await axios.post('http//localhost:3001/login', { key });
             setMessage(responce.data.message);
+
+            //fix
             navigate('/mainPage');
         } catch(error){
             setMessage('Authorization failed');
@@ -18,16 +22,61 @@ const Authorizations = () => {
     };
 
     return(
-        <div>
-            <input
-                type='text'
-                value={key}
-                onChange={(e) => setKey(e.target.value)}
-                placeholder='Enter key'
-            />
-            <button onClick={handleAuthorization}>Enter</button>
-            <p>{message}</p>
-        </div>
+        <Container component="main" maxWidth="xs">
+            <Box
+                sx={{
+                    marginTop: 8,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                    borderColor: '',
+                    justifyContent: 'center',
+                    minHeight: '70vH',
+                    color: 'white',
+                }}
+                >
+                    <Typography component="h1" variant="h5" sx={{ fontFamily: 'PT Sans'}}>
+                        Authorization
+                    </Typography>
+
+                    {/* Fix this */}
+                    <Box component="form" noValidate sx={{ mt:1 }}>
+                        <TextField
+                            margin='normal'
+                            required
+                            fullWidth
+                            id='key'
+                            label='Enter Key'
+                            name='key'
+                            autoFocus
+                            value={key}
+                            onChange={(e) => setKey(e.target.value)}
+                            sx={{
+                                fontFamily: 'PT Sans',
+                                color: 'white'
+                            }}
+                    />
+
+                    {/* Fix this */}
+                    <Button
+                        fullWidth
+                        variant='contained'
+                        sx={{ 
+                            mt: 3, 
+                            mb: 2,  
+                            fontFamily: 'PT Sans',
+                            borderColor: 'green'
+                        }}
+                        onClick={handleAuthorization}
+                    >
+                        Enter
+                    </Button>
+                    <Typography variant='body2' color="error" align='center'>
+                        {message}
+                    </Typography>
+                </Box>
+            </Box>
+        </Container>
     );
 };
 
