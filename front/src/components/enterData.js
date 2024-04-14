aa023cf8c1c42645cc3863b8c6db1e099efac0fc
aquamaryne <67910638+aquamaryne@users.noreply.github.com>
2024-04-14 20:03:06 +0300

    anyway need to complete POST req

--- front/src/components/enterData.js
+++ front/src/components/enterData.js
@@ -1,105 +1,74 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React, { useState } from 'react';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography'
-import { styled } from '@mui/system';
+import { Snackbar } from '@mui/material';
+import { Alert } from '@mui/material';
 import "../css/form.css";
-
-
-const ModernTextField = styled(TextField)({
-    "& .MuiOutlinedInput-root": {
-        borderRadius: "0px",
-        backgroundColor: "white",
-        border: "2px solid black",
-        "&:hover fieldset": {
-            borderColor: "transparent", // убираем границу при наведении
-        },
-        "&.Mui-focused fieldset": {
-            borderColor: "black", // меняем цвет границы при фокусировке
-        },
-    },
-    "& .MuiInputLabel-root": {
-        color: "black",
-        fontWeight: "bold",
-    },
-    "& .MuiInputBase-input": {
-        color: "black",
-    },
-    "& .MuiFormHelperText-root": {
-        color: "gray",
-    },
-    "& .MuiOutlinedInput-notchedOutline": {
-        border: "none",
-    },
-    "& .MuiOutlinedInput-input": {
-        padding: "16px",
-    },
-    transition: "background-color 0.3s, border-color 0.3s, box-shadow 0.3s",
-});
-
-const buttonStyles = (clicked) => ({
-    border: 2,
-    fontFamily: 'Daikon',
-    fontWeight: 'bold',
-    borderRadius: 0,
-    color: clicked ? "white" : "black",
-    backgroundColor: clicked ? '#191970' : 'transparent',
-    '&:hover': {
-        backgroundColor: clicked ? '#191970' : '#191970',
-        color: clicked ? 'white' : 'white',
-        borderColor: 'orange',
-        boxShadow: '-4px 2px 2px 0 purple',
-    },
-    transition: 'box-shadow 0.3s'
-});
 
 const EnterDataForm = () => {
     const [clicked, setClicked] = useState(false);
-    const [employeers, setEmployeers] = useState([]);
     const [formData, setFormData] = useState({});
+    const [notification, setNotification] = useState({ open: false, message: '', severity: 'success'});
+
 
     const handleClick = () => {
         setClicked(prevClicked => !prevClicked);
     };
 
-    const handleSubmit = (event) => {
+    const handleSubmit = () => {
         handleSave();
-    }
+    };
+
+    const handleCloseNotifiaction = () => {
+        setNotification({...notification, open: false});
+    };
+
     //api for POST data in db
-    const handleSave = () => {
-        axios.post('http://localhost:3001/employeers', formData) 
-            .then(res => {
-                setEmployeers(res.data);
-                console.log('Data saved successfully:', res.data);
-            })
-            .catch(err => console.error('Error saving data:', err)); 
+    const handleSave = async () => {
+        try{
+            const responce = await fetch(`http://localhost:3001/employeers`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(formData),
+            });
+            if(!responce.ok){
+                throw new Error('Error to send req');
+            }
+            setNotification({open: true, message: 'Дані були успішно внесені', severity: 'success'});
+            setFormData({});
+        } catch(error){
+            console.error('Error saving data', error);
+            setNotification({open: true, message: 'Помилка при внесені даних', severity: 'error'})
+        }
     };
 
     const handleChange = (event) => {
-        const { sname, fname, value } = event.target;
+        const { name, value } = event.target;
         setFormData(prevFormData => ({
             ...prevFormData,
-            [sname]: value,
-            [fname]: value
+            [name]: value
         }))
     }
 
     return(
         <div>
-            <Button onClick={handleClick} sx={buttonStyles(clicked)}>
+            <Button onClick={handleClick}>
                 <Typography>Внесееня даних</Typography>
             </Button>  
             {clicked && (
                 <div>
                     <form onSubmit={handleSubmit}>
                         <Grid container spacing={2}>
-                            <Grid item xs={12} md={6}>
-                                <ModernTextField 
+                        <Grid item xs={12} md={6}>
+                                <TextField 
                                     fullWidth
                                     label="Ім'я"
                                     name='name'
+                                    value={formData.fname}
                                     onChange={handleChange}
                                     variant="outlined"
                                     size="small"
@@ -107,10 +76,11 @@ const EnterDataForm = () => {
                                 />
                             </Grid>
                             <Grid item xs={12} md={6}>
-                                <ModernTextField 
+                                <TextField 
                                     fullWidth
                                     label="Прізвище"
                                     name='name'
+                                    value={formData.sname}
                                     onChange={handleChange}
                                     variant="outlined"
                                     size="small"
@@ -122,6 +92,11 @@ const EnterDataForm = () => {
                 </form>
             </div>
         )}
+        <Snackbar open={notification.open} autoHideDuration={600} onClose={handleCloseNotifiaction}>
+            <Alert elevation={6} variant='filled' onClose={handleCloseNotifiaction} severity={notification.severity}>
+                {notification.message};
+            </Alert>
+        </Snackbar>
     </div> 
     )
 };
