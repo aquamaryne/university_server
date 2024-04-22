1410302187bd1f5d649740d815e58f1e1a5ef963
aquamaryne <67910638+aquamaryne@users.noreply.github.com>
2024-04-22 13:48:47 +0300

    added onChange for military

--- front/src/data/military.data.js
+++ front/src/data/military.data.js
@@ -44,14 +44,14 @@ const MilitaryData = () => {
     return(
         <div>
             <form onSubmit={handleMilitarySubmit}>
-                <input type='text' name="accounting_group" value={militaryAppearanceData.accounting_group}></input>
-                <input type='text' name="accounting_category" value={militaryAppearanceData.accounting_category}></input>
-                <input type='text' name="depot" value={militaryAppearanceData.depot}></input>
-                <input type='text' name="military_rank" value={militaryAppearanceData.military_rank}></input>
-                <input type='text' name="military_accounting_specialty" value={militaryAppearanceData.military_accounting_specialty}></input>
-                <input type='text' name="num" value={militaryAppearanceData.num}></input>
-                <input type='text' name="suitability_for_military_service" value={militaryAppearanceData.suitability_for_military_service}></input>
-                <input type='text' name="name_of_the_military_office_at_the_place_of_residence" value={militaryAppearanceData.name_of_the_military_office_at_the_place_of_residence}></input>
+                <input type='text' name="accounting_group" value={militaryAppearanceData.accounting_group} onChange={handleMilitaryChange}></input>
+                <input type='text' name="accounting_category" value={militaryAppearanceData.accounting_category} onChange={handleMilitaryChange}></input>
+                <input type='text' name="depot" value={militaryAppearanceData.depot} onChange={handleMilitaryChange}></input>
+                <input type='text' name="military_rank" value={militaryAppearanceData.military_rank} onChange={handleMilitaryChange}></input>
+                <input type='text' name="military_accounting_specialty" value={militaryAppearanceData.military_accounting_specialty} onChange={handleMilitaryChange}></input>
+                <input type='text' name="num" value={militaryAppearanceData.num} onChange={handleMilitaryChange}></input>
+                <input type='text' name="suitability_for_military_service" value={militaryAppearanceData.suitability_for_military_service} onChange={handleMilitaryChange}></input>
+                <input type='text' name="name_of_the_military_office_at_the_place_of_residence" value={militaryAppearanceData.name_of_the_military_office_at_the_place_of_residence} onChange={handleMilitaryChange}></input>
             </form>
         </div>
     )
