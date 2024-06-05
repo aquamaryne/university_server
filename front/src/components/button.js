d59e6f1d2e805553dcddd1aa03ffcba8140c19f7
aquamaryne <67910638+aquamaryne@users.noreply.github.com>
2024-06-05 12:39:30 +0300

    updates

--- front/src/components/button.js
+++ front/src/components/button.js
@@ -2,8 +2,20 @@ import React from "react";
 import { Button } from "@mui/material";
 
 const SaveButton = () => {
+
+    const handleSubmit = (event) => {
+        fetch('http://localhost:3001', {
+            method: 'POST',
+        })
+        .then(function(responce) {
+            console.log(responce);
+            return responce.json();
+        })
+
+        event.preventDefault();
+    }
     return(
-        <Button>Зберегти</Button>
+        <Button onClick={handleSubmit}>Зберегти</Button>
     )
 }
 
