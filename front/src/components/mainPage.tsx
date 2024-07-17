a50c7464790e21f7f3f559250223f5517df5e5e4
aquamaryne <67910638+aquamaryne@users.noreply.github.com>
2024-07-17 09:43:01 +0300

    navbar changes

---
+++ front/src/components/mainPage.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Grid } from "@mui/material";
+import Archieve from "./archieve";
+
+const MainPage: React.FC = () => {
+    return <Archieve />
+}
+
+export default MainPage;
---
+++ front/src/components/print.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Print: React.FC = () => {
+    return <h1>1</h1>
+};
+
+export default Print;
