86ad90ac53b9b4e8cc081c540b73e7ae8f5ddb75
aquamaryne <67910638+aquamaryne@users.noreply.github.com>
2024-09-06 16:38:09 +0300

    cors updates

--- personnel_server/src/main.ts
+++ personnel_server/src/main.ts
@@ -3,9 +3,18 @@ import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { join } from 'path';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  
+  const corsOptions: CorsOptions = {
+    origin: 'http://localhost:3000',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  };
+
+  app.enableCors(corsOptions);
 
   const config = new DocumentBuilder()
     .setTitle('API Test')
