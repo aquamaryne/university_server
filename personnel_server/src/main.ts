8450c4f694d9ec5e36038d86dfb2ab32002de103
aquamaryne <67910638+aquamaryne@users.noreply.github.com>
2024-09-06 16:01:08 +0300

    cors dis

--- personnel_server/src/main.ts
+++ personnel_server/src/main.ts
@@ -7,7 +7,6 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  app.enableCors();
   const config = new DocumentBuilder()
     .setTitle('API Test')
     .setDescription('API для тестирование запросов в приложение "Отдел Кадров"')
