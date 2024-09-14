8fa06f2656087f86d4a7e977a3ac75d8fae47d88
aquamaryne <67910638+aquamaryne@users.noreply.github.com>
2024-09-14 21:42:08 +0300

    added form in electron

--- desktop/src/index.js
+++ desktop/src/index.js
@@ -1,36 +1,32 @@
 const { app, BrowserWindow, ipcMain } = require('electron');
 const path = require('path');
-const os = require('os');
-
-let mainWindow;
 
 function createWindow(){
-  mainWindow = new BrowserWindow({
+  const win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
-      contextIsolation: true,
-      nodeIntegration: false,
-    }
+        preload: path.join(__dirname, 'preload.js'),
+        nodeIntegration: true,
+        contextIsolation: false,
+    },
+    autoHideMenuBar: true,
   });
 
-  mainWindow.loadURL('212.111.203.173:3000');
+  win.loadFile('index.html');
 }
 
 app.whenReady().then(() => {
-  createWindow
-
-  app.on('activate', () => {
-    if(mainWindow === null){
-      createWindow();
-    }
-  })
+    createWindow();
+    app.on('activate', () => {
+        if(BrowserWindow.getAllWindows().length === 0){
+            createWindow();
+        }
+    });
 });
 
 app.on('window-all-closed', () => {
-  if(process.platform !== 'darwin'){
-    app.quit();
-  }
-});
-
+    if(process.platform !== 'darwin') {
+        app.quit();
+    }
+})
