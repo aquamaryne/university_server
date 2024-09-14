cc6961f87cdc5e22c3da4365f27fdb39e9416fd2
aquamaryne <67910638+aquamaryne@users.noreply.github.com>
2024-09-14 22:36:52 +0300

    added icon

--- desktop/src/index.js
+++ desktop/src/index.js
@@ -10,6 +10,7 @@ function createWindow(){
         nodeIntegration: true,
         contextIsolation: false,
     },
+    icon: path.join(__dirname, 'assets', 'file.png'),
     autoHideMenuBar: true,
   });
 
