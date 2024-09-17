cccb1a644b53011e9223c6cdd75fa59f7ee877cb
aquamaryne <67910638+aquamaryne@users.noreply.github.com>
2024-09-17 13:53:24 +0300

    update

--- front/src/components/layout.tsx
+++ front/src/components/layout.tsx
@@ -1,12 +1,17 @@
 import MainPage from "./mainPage"
+import { useLocation } from "react-router-dom";
 
 const Layout = ({ children } : { children: React.ReactNode }) => {
+    const location = useLocation();
+
     return(
         <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh'}}>
-            <header>
-                <MainPage />
-            </header>
-            <main style={{ flex: 1 }}>
+            {location.pathname !== '/' && (
+                <header>
+                    <MainPage />
+                </header>
+            )}
+            <main style={{ flex: 1, paddingTop: location.pathname !== '/' ? '20px' : '0px'}}>
                 {children}
             </main>
         </div>
