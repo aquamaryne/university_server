66e2970974bd271389b3a7fabc909da2f5ce54df
aquamaryne <67910638+aquamaryne@users.noreply.github.com>
2024-09-18 17:46:04 +0300

    deleted routes

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -2,7 +2,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MainPage from './components/mainPage';
 import ArchieveRoute from './routes/archieve';
 import FormRoute from './routes/form';
-import WitnessPrintRoute from './routes/witness_print';
 import PersonalCardRoute from './routes/personal_card';
 import StatisticRoute from './routes/statisctic';
 import WitnessRoute from './routes/witness';
@@ -30,7 +29,6 @@ function App() {
             </div> */ }
             <ArchieveRoute />
             <FormRoute />
-            <WitnessPrintRoute />
             <PersonalCardRoute />
             <StatisticRoute />
             <WitnessRoute />
