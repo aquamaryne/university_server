cba6a296dcc698a5fd524a4276e2fca7a1394d67
aquamaryne <67910638+aquamaryne@users.noreply.github.com>
2024-09-20 15:35:16 +0300

    cron update

--- personnel_server/src/backup/backup.service.ts
+++ personnel_server/src/backup/backup.service.ts
@@ -33,7 +33,7 @@ export class BackupService implements OnModuleDestroy {
         })
     }
 
-    @Cron(CronExpression.EVERY_10_MINUTES)
+    @Cron(CronExpression.EVERY_DAY_AT_MIDNIGHT)
     async backupDatabase(){
         console.log('Backup task started', new Date());
         const timeStamp = new Date().toISOString().replace(/[:]/g, '-');
