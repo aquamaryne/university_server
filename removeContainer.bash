5da7889973ba71ca2beb8f6953b4fe3b06c8cc3d
aquamaryne <67910638+aquamaryne@users.noreply.github.com>
2024-09-25 23:44:50 +0300

    docker update

--- removeContainer.bash
+++ removeContainer.bash
@@ -6,7 +6,7 @@ remove_container(){
     local container
     containers=$(docker ps -aq)
 
-    if [[ -n "$containers" ]] then
+    if [[ -n "$containers" ]]; then
         printf "Removing all containers ...\n"
         docker rm -f "$containers"
     else 
@@ -18,7 +18,7 @@ remove_images(){
     local images
     images=$(docker images -q)
 
-    if [[ -n "$images" ]] then
+    if [[ -n "$images" ]]; then
         printf "Removing all images...\n"
         docker rmi -f "$images"
     else
