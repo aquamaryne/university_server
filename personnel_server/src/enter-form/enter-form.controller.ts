ecc232eb420cc5912eefe02e84c2719fe8289c2c
aquamaryne <67910638+aquamaryne@users.noreply.github.com>
2024-10-15 14:32:44 +0300

    api updates

--- personnel_server/src/enter-form/enter-form.controller.ts
+++ personnel_server/src/enter-form/enter-form.controller.ts
@@ -1,6 +1,6 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Controller, Get, Render, Res, Post, Body } from '@nestjs/common';
 import { Public } from 'src/api_key/public';
-
+import { Response } from 'express';
 @Controller('enter-form')
 export class EnterFormController {
     @Get()
@@ -8,5 +8,20 @@ export class EnterFormController {
     @Render('form')
     apiKeyForm(){
         return {};
+    }
+
+    @Post('/submit_key')
+    submitKey(@Body('apiKey') apiKey: string, @Res() res: Response){
+        res.cookie('authKey', apiKey, { httpOnly: true });
+        return res.status(200).json({ message: 'API key saved' });
+    }
+
+    @Get('/validate_key')
+    validateKey(@Res() res: Response){
+        const apiKey = res.req.cookies['authKey'];
+        if(apiKey){
+            return res.status(200).json({ message: 'API deleted' });
+        }
+        return res.status(401).json({ message: 'API key not found' });
     }
 }
