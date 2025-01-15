5b7a153f7f730ae9cade464bf7c223fa4d2b1696
aquamaryne <67910638+aquamaryne@users.noreply.github.com>
2025-01-15 15:44:01 +0200

    update form

--- front/src/view/form/printForm.tsx
+++ front/src/view/form/printForm.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Table, TableCell, TableBody, TableContainer, TableHead, TableRow, Paper, Button } from "@mui/material";
 import ReactToPrint, { useReactToPrint } from 'react-to-print';
+import PrintIcon from '@mui/icons-material/Print';
 
 const PrintForm: React.FC = () => {
     const componentRef = React.useRef<HTMLDivElement>(null);
@@ -12,28 +13,21 @@ const PrintForm: React.FC = () => {
     });
 
     return (
-        <div>
-            <Button
-                variant='contained'
-                onClick={handlePrint}
-                color='primary'
-                sx={{ marginBottom: '2px' }}
-                >
-                Друк
-            </Button>
+        <div style={{ justifyContent: 'center', padding: '16px' }}>
             <TableContainer 
                 ref={componentRef}
                 component={Paper}
                 sx={{
                     '@media print': {
+                        transform: 'scale(0.95)',
+                        transformOrigin: 'top left',
                         border: '1px solid black',
-                        boxShadow: 'none',
                         margin: '0',
                         width: '100%',
                     },
                     '@page': {
                         size: 'A4 landscape',
-                        margin: '0',
+                        margin: '10mm',
                     }
                 }}
             >
@@ -42,83 +36,51 @@ const PrintForm: React.FC = () => {
                     sx={{
                         tableLayout: 'fixed',
                         borderCollapse: 'collapse',
-                        width: '100%',
+                        padding: 'center',
                         '& th, & td': {
                             border: '1px solid black',
                             wordWrap: 'break-word',
                             whiteSpace: 'normal',
                             padding: '4px',
                             fontSize: '0.8rem',
-                            textAlign: 'left',
+                            textAlign: 'center',
                             verticalAlign: 'middle',
                         },
                         '@media print': {
                             '& th, & td': {
-                                fontSize: '10px',
+                                fontSize: '7px',
                             }
                         }
                     }}
                 >
                     <TableHead>
                         <TableRow>
-                            <TableCell rowSpan={2} align="center" sx={{ fontWeight: 'bold', border: '1px solid black', textAlign: 'center', verticalAlign: 'middle', fontSize: '0.85rem' }}>
-                                № п/п
-                            </TableCell>
-                            <TableCell rowSpan={2} align="center" sx={{ fontWeight: 'bold', border: '1px solid black', textAlign: 'center', verticalAlign: 'middle', fontSize: '0.85rem' }}>
-                                Прізвище, ім'я та по-батькові
-                            </TableCell>
-                            <TableCell rowSpan={2} align="center" sx={{ fontWeight: 'bold', border: '1px solid black', textAlign: 'center', verticalAlign: 'middle', fontSize: '0.85rem' }}>
-                                Посада
-                            </TableCell>
-                            <TableCell rowSpan={2} align="center" sx={{ fontWeight: 'bold', border: '1px solid black', textAlign: 'center', verticalAlign: 'middle', fontSize: '0.85rem' }}>
-                                Дата вступу на посаду
-                            </TableCell>
-                            <TableCell rowSpan={2} align="center" sx={{ fontWeight: 'bold', border: '1px solid black', textAlign: 'center', verticalAlign: 'middle', fontSize: '0.85rem' }}>
-                                Дисципліна, яку читає
-                            </TableCell>
-                            <TableCell rowSpan={2} align="center" sx={{ fontWeight: 'bold', border: '1px solid black', textAlign: 'center', verticalAlign: 'middle', fontSize: '0.85rem' }}>
-                                Штат чи сумісник
-                            </TableCell>
-                            <TableCell rowSpan={2} align="center" sx={{ fontWeight: 'bold', border: '1px solid black', textAlign: 'center', verticalAlign: 'middle', fontSize: '0.85rem' }}>
-                                Вчене звання
-                            </TableCell>
-                            <TableCell rowSpan={2} align="center" sx={{ fontWeight: 'bold', border: '1px solid black', textAlign: 'center', verticalAlign: 'middle', fontSize: '0.85rem' }}>
-                                Вчений ступінь
-                            </TableCell>
-                            <TableCell colSpan={2} align="center" sx={{ fontWeight: 'bold', border: '1px solid black', textAlign: 'center', verticalAlign: 'middle', fontSize: '0.85rem' }}>
-                                Стаж науково-пед. роботи
-                            </TableCell>
-                            <TableCell rowSpan={2} align="center" sx={{ fontWeight: 'bold', border: '1px solid black', textAlign: 'center', verticalAlign: 'middle', fontSize: '0.85rem' }}>
-                                Рік останнього підвищення кваліфікації
-                            </TableCell>
-                            <TableCell colSpan={3} align="center" sx={{ fontWeight: 'bold', border: '1px solid black', textAlign: 'center', verticalAlign: 'middle', fontSize: '0.85rem' }}>
-                                Загальні дані
-                            </TableCell>
-                            <TableCell rowSpan={2} align="center" sx={{ fontWeight: 'bold', border: '1px solid black', textAlign: 'center', verticalAlign: 'middle', fontSize: '0.85rem' }}>
-                                Почесне звання
-                            </TableCell>
-                            <TableCell rowSpan={2} align="center" sx={{ fontWeight: 'bold', border: '1px solid black', textAlign: 'center', verticalAlign: 'middle', fontSize: '0.85rem' }}>
-                                Іноземна мова, якою володіє
-                            </TableCell>
-                            <TableCell rowSpan={2} align="center" sx={{ fontWeight: 'bold', border: '1px solid black', textAlign: 'center', verticalAlign: 'middle', fontSize: '0.85rem' }}>
-                                Звільнення, у який навч. заклад
-                            </TableCell>
-                            <TableCell rowSpan={2} align="center" sx={{ fontWeight: 'bold', border: '1px solid black', textAlign: 'center', verticalAlign: 'middle', fontSize: '0.85rem' }}>
-                                Дата закінчення трудового договору або контракту
-                            </TableCell>
+                            <TableCell rowSpan={2}>№ п/п</TableCell>
+                            <TableCell rowSpan={2}>Прізвище, ім'я та по-батькові</TableCell>
+                            <TableCell rowSpan={2}>Посада</TableCell>
+                            <TableCell rowSpan={2}>Дата вступу на посаду</TableCell>
+                            <TableCell rowSpan={2}>Дисципліна, яку читає</TableCell>
+                            <TableCell rowSpan={2}>Штат чи сумісник</TableCell>
+                            <TableCell rowSpan={2}>Вчене звання</TableCell>
+                            <TableCell rowSpan={2}>Вчений ступінь</TableCell>
+                            <TableCell colSpan={2}>Стаж науково-пед. роботи</TableCell>
+                            <TableCell rowSpan={2}>Рік останнього підвищення кваліфікації</TableCell>
+                            <TableCell colSpan={3}>Загальні дані</TableCell>
+                            <TableCell rowSpan={2}>Почесне звання</TableCell>
+                            <TableCell rowSpan={2}>Іноземна мова, якою володіє</TableCell>
+                            <TableCell rowSpan={2}>Звільнення, у який навч. заклад</TableCell>
+                            <TableCell rowSpan={2}>Дата закінчення трудового договору або контракту</TableCell>
                         </TableRow>
                         <TableRow>
-                            <TableCell align="center" sx={{ fontWeight: 'bold', border: '1px solid black', textAlign: 'center', verticalAlign: 'middle' }}>Загальний</TableCell>
-                            <TableCell align="center" sx={{ fontWeight: 'bold', border: '1px solid black', textAlign: 'center', verticalAlign: 'middle' }}>У даному ВНЗ</TableCell>
-                            <TableCell align="center" sx={{ fontWeight: 'bold', border: '1px solid black', textAlign: 'center', verticalAlign: 'middle' }}>Рік народження</TableCell>
-                            <TableCell align="center" sx={{ fontWeight: 'bold', border: '1px solid black', textAlign: 'center', verticalAlign: 'middle' }}>Стать</TableCell>
-                            <TableCell align="center" sx={{ fontWeight: 'bold', border: '1px solid black', textAlign: 'center', verticalAlign: 'middle' }}>
-                                Освіта (який навч. заклад закінч. і коли)
-                            </TableCell>
+                            <TableCell align="center">Загальний</TableCell>
+                            <TableCell align="center">У даному ВНЗ</TableCell>
+                            <TableCell align="center">Рік народження</TableCell>
+                            <TableCell align="center">Стать</TableCell>
+                            <TableCell align="center">Освіта (який навч. заклад закінч. і коли)</TableCell>
                         </TableRow>
                         <TableRow>
                             {Array(18).fill(0).map((_, index) => (
-                                <TableCell key={index} align="center" sx={{ fontWeight: 'bold', border: '1px solid black' }}>
+                                <TableCell key={index} align="center">
                                     {index + 1}
                                 </TableCell>
                             ))}
@@ -129,6 +91,15 @@ const PrintForm: React.FC = () => {
                     </TableBody>
                 </Table>
             </TableContainer>
+            <Button
+                variant='contained'
+                onClick={handlePrint}
+                color='primary'
+                sx={{ marginTop: '30px' }}
+                endIcon={<PrintIcon />}
+            >
+                Друк
+            </Button>
         </div>
     )
 }
